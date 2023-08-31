71bb38a3d0af9d2488c80cc5c6a54f85464f3fc0
zares <sergiozares@gmail.com>
2023-08-31 11:05:48 +0300

    Work on Turbo-Frame section.
    Turbo Frame Basics.
    
    ---
    Turbo Frame can help us treat part of our page like HTML iframe element.
    Turbo Frames allow predefined parts of a page to be updated on request.
    Any links and forms inside a frame are captured, and the frame contents
    automatically updated after receiving a response.
    With Turbo Frame, we can update segment of our page without reloading
    the whole page.
    ---
    
    1. Add Turbo Frame to Turbo Frame Index page.
    2. It contains a link, if we click the link, Turbo will capture the event,
    and load the url to update the content of the frame.
    3. Update List page file to wrap the list div in turbo-frame id="task-list".

--- app/resources/views/turbo-frame/index.blade.php
+++ app/resources/views/turbo-frame/index.blade.php
@@ -5,5 +5,8 @@
 @section('content')
     <div class="w-full max-w-3xl mx-auto px-4">
         <h1 class="text-3xl sm:text-4xl mb-6">Turbo Frame Index Page</h1>
+        <turbo-frame id="task-list">
+            <a class="btn-blue" href="{{ route('turbo-frame.list') }}">Click to load...</a>
+        </turbo-frame>
     </div>
 @stop
--- app/resources/views/turbo-frame/list-page.blade.php
+++ app/resources/views/turbo-frame/list-page.blade.php
@@ -5,14 +5,16 @@
 @section('content')
     <div class="w-full max-w-3xl mx-auto px-4">
         <h1 class="text-3xl sm:text-4xl mb-6">Task List</h1>
-        <div class="bg-white rounded-lg border mb-4">
-            <ul class="divide-y-2 divide-gray-100" id="task-list-ul">
-                @foreach ($tasks as $task)
-                    <li class="p-3 items-center">
-                        @include('turbo-frame.task-detail', ['task' => $task])
-                    </li>
-                @endforeach
-            </ul>
-        </div>
+        <turbo-frame id="task-list">
+            <div class="bg-white rounded-lg border mb-4">
+                <ul class="divide-y-2 divide-gray-100" id="task-list-ul">
+                    @foreach ($tasks as $task)
+                        <li class="p-3 items-center">
+                            @include('turbo-frame.task-detail', ['task' => $task])
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </turbo-frame>
     </div>
 @stop
