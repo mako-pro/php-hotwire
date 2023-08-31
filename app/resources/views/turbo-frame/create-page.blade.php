a95abfdcdee08ac08f3ea738c24b9a162871bfd0
zares <sergiozares@gmail.com>
2023-08-31 14:30:43 +0300

    Work on Turbo-Frame section.
    Fix Form for the standard Create Task page.
    
    ---
    The logic is simple, we only render turbo-frame element,
    if the HTTP request has Turbo-Frame header.
    ---
    
    So, now:
    
    1. If we create task on /turbo-frame/ page, will be returned success message in turbo-frame.
    2. If we create task on /turbo-frame/create/ page, will be returned 302 response with success flash message.

--- app/resources/views/turbo-frame/create-page.blade.php
+++ app/resources/views/turbo-frame/create-page.blade.php
@@ -5,8 +5,12 @@
 @section('content')
     <div class="w-full max-w-3xl mx-auto px-4">
         <h1 class="text-3xl sm:text-4xl mb-6">Create Task</h1>
-        <turbo-frame id="task-create">
+        @if ($frame = turbo_frame())
+            <turbo-frame id="{{ $frame }}">
+                @include('turbo-frame.form.create')
+            </turbo-frame>
+        @else
             @include('turbo-frame.form.create')
-        </turbo-frame>
+        @endif
     </div>
 @stop
