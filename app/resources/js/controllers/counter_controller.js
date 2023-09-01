e202423b1a7786b7f4b1862a42dc4de48312100f
zares <sergiozares@gmail.com>
2023-09-01 13:20:41 +0300

    Work on Stimulus-Basics section.
    1. Modify Counter Controller to works as click counter with multiple DOM elements.

--- app/resources/js/controllers/counter_controller.js
+++ app/resources/js/controllers/counter_controller.js
@@ -1,7 +1,49 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    connect() {
-        this.element.innerHTML = 'Hello From Counter Controller!';
+    static values = {
+        count: { type: Number, default: 0 },
+    };
+
+    static targets = [
+        'count',
+        'initialDiv',
+        'progressDiv'
+    ];
+
+    static classes = ['hide'];
+
+    initialize(){
+        console.log('Initialize fired...');
     }
+
+    connect() {
+        console.log('Connect fired...');
+        this.countTarget.innerText = this.countValue;
+    }
+
+    disconnect() {
+        console.log('Disconnect fired...');
+    }
+
+    countValueChanged(value, previousValue) {
+        console.log(`${previousValue} changed to ${value}`);
+        if (value === 1) {
+            this.initialDivTarget.classList.add(this.hideClass);
+            this.progressDivTarget.classList.remove(this.hideClass);
+        }
+    }
+
+    increment() {
+        this.countValue++;
+        this.countTarget.innerText = this.countValue;
+    }
+
+    reset() {
+        // cleanup for page cache
+        this.countValue = 0;
+        this.initialDivTarget.classList.remove(this.hideClass);
+        this.progressDivTarget.classList.add(this.hideClass);
+    }
+
 }
--- app/resources/views/stimulus-basic/counter.blade.php
+++ app/resources/views/stimulus-basic/counter.blade.php
@@ -5,6 +5,41 @@
 @section('content')
     <div class="w-full max-w-3xl mx-auto px-4">
         <h1 class="text-3xl sm:text-4xl mb-6">Counter</h1>
-        <div data-controller="counter"></div>
+        <p class="mb-4">
+            <button class="btn-blue"
+                data-controller="counter"
+                data-action="click->counter#increment turbo:before-cache@window->counter#reset"
+                data-counter-hide-class="hidden"
+            >
+                <span data-counter-target="initialDiv">Click Me</span>
+                <span data-counter-target="progressDiv" class="hidden">
+                    You clicked <span class="font-bold px-1" data-counter-target="count"></span> times!
+                </span>
+            </button>
+        </p>
+        <p class="mb-4">
+            <button class="btn-green"
+                data-controller="counter"
+                data-action="click->counter#increment turbo:before-cache@window->counter#reset"
+                data-counter-hide-class="hidden"
+            >
+                <span data-counter-target="initialDiv">Click Me</span>
+                <span data-counter-target="progressDiv" class="hidden">
+                    You clicked <span class="font-bold px-1" data-counter-target="count"></span> times!
+                </span>
+            </button>
+        </p>
+        <p class="mb-4">
+            <button class="btn-red"
+                data-controller="counter"
+                data-action="click->counter#increment turbo:before-cache@window->counter#reset"
+                data-counter-hide-class="hidden"
+            >
+                <span data-counter-target="initialDiv">Click Me</span>
+                <span data-counter-target="progressDiv" class="hidden">
+                    You clicked <span class="font-bold px-1" data-counter-target="count"></span> times!
+                </span>
+            </button>
+        </p>
     </div>
 @stop
