2f5d25f4456fb5f822ef7fd5d8c44bb812aed7fb
zares <sergiozares@gmail.com>
2023-09-02 11:00:13 +0300

    Work on Stimulus-Advanced section.
    Build Stimulus Controller for Flash Message.
    
    ---
    tailwindcss-stimulus-components is A set of StimulusJS components for
    TailwindCSS apps similar to Bootstrap JS components.
    ---
    
    1. Install tailwindcss-stimulus-components.
    2. Register the alert controller from the tailwindcss-stimulus-components.
    3. Update messages file to use the alert controller.
    
    Notes:
    1. If we want to change the JS behavior, we can create a alert_controller
       which inherit from the tailwindcss-stimulus-components Alert.
    2. This controller will supports custom values.

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -2,7 +2,10 @@ import "@hotwired/turbo";
 
 import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+import { Alert } from "tailwindcss-stimulus-components";
 
 window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 window.Stimulus.load(definitionsFromContext(context));
+
+window.Stimulus.register('alert', Alert);
--- app/resources/views/stimulus-advanced/messages.blade.php
+++ app/resources/views/stimulus-advanced/messages.blade.php
@@ -10,10 +10,17 @@
             bg-amber-500
         @endif"
         data-turbo-temporary="true"
+        data-controller="alert"
+        data-alert-remove-delay-value="0"
         role="alert"
     >
         <span class="inline-block align-middle mr-8">
             {{ $message }}
         </span>
+        <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
+            data-action="alert#close"
+        >
+            <span>×</span>
+        </button>
     </div>
 @endforeach
